b830b0a94ac0564a1221d59f645e11f40565ea12
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-10-02 18:09:48 +0300

    Configured the transition from one graphic window to another

--- src/main/java/example/myapp1/RunApplication.java
+++ src/main/java/example/myapp1/RunApplication.java
@@ -7,9 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class RunApplication extends Application {
+public class RunApplication extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(RunApplication.class.getResource("registration.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 700);
         stage.setTitle("MyApp1");
@@ -19,7 +21,8 @@ public class RunApplication extends Application {
         fxmlLoader.setController(new InputUserInDbController());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
